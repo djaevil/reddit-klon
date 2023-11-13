60194c26b815d33aaedd464d559d833b31cac49f
djaevil <djaevil1@gmail.com>
2023-11-13 16:08:07 +0100

    Entry 2.2
    Fort. tidigare arbete. Utvecklat js kod men inte klart ännu.

--- script.js
+++ script.js
@@ -14,8 +14,19 @@ if (storedData) {
     
     parsedData.array.forEach(post => {
         let postElement = document.createElement("div");
-        postElement.textContainer = post.title;
+        let postTitle = document.createElement("h3");
+        let postBody = document.createElement("p");
+        let postTags = document.createElement("span");
+
+        postElement.classList.add("post-main");
+        postTitle.classList.add("post-title");
+        postBody.classList.add("post-body");
+        postTags.classList.add("post-tags");
+
         outputPosts.appendChild(postElement);
+        postElement.appendChild(postTitle, postBody, postTags);
     });
+} else {
+    console.log("No data found in local storage");
 }
             
