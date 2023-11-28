d1a9d3291cfae121a100699090f1d57592d61398
djaevil <djaevil1@gmail.com>
2023-11-28 18:02:45 +0100

    Entry 4
    Fort. på js koden men den funkar inte än.

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-fetch('https://dummyjson.com/posts')
-.then(res => res.json())
-.then(data => {
+fetch("https://dummyjson.com/posts")
+  .then((res) => res.json())
+  .then((data) => {
     localStorage.setItem("fetchedData", JSON.stringify(data));
     console.log("Data stored locally:", data);
-})
-.catch(error => console.error("Error fetching data:", error));
+  })
+  .catch((error) => console.error("Error fetching data:", error));
 
-const storedData = localStorage.getItem("fetchedData");
+var storedData = localStorage.getItem("fetchedData");
+var feedPosts = document.getElementById("feed-posts");
+var parsedData = JSON.parse(storedData);
 
-if (storedData) {
-    const parsedData = JSON.parse(storedData);
-    const feedPosts = document.getElementById("feed-posts");
-    
-    parsedData.forEach(post => {
-        const postElement = document.createElement("div");
-        const postTitle = document.createElement("h3");
-        const postBody = document.createElement("p");
-        const postTags = document.createElement("ul")
-
+for (let i = 0; i <= 30; i++) {
+    if (parsedData.posts[i] === i) {
+        var postElement = document.createElement("div");
+        var postTitle = document.createElement("h3");
+        var postBody = document.createElement("p");
+        var postTags = document.createElement("ul");
+      
         postElement.classList.add("post-main");
         postTitle.classList.add("post-title");
         postBody.classList.add("post-body");
         postTags.classList.add("post-tags");
-
-        postTitle.textContent = post.title;
-        postBody.textContent = post.body;
-        postTags.textContent = post.tags;
-        
+      
+        postTitle.textContent = parsedData.posts.title;
+        postBody.textContent = parsedData.posts.body;
+        postTags.textContent = parsedData.posts.tags;
+      
+        console.log(parsedData.posts.title);
+      
         feedPosts.append(postElement);
         postElement.append(postTitle, postBody, postTags);
-    });
-} else {
-    console.log("No data found in local storage");
+    }
 }
-            
