09bab4e3852bb5a2b365052a3e6048494c7e14cc
djaevil <djaevil1@gmail.com>
2023-11-29 19:44:40 +0100

    Entry 5
    Jag har skrivit färdigt js koden som fetchar data från DummyJSON och sedan använder datan för att skapa inlägg på hemsidan.
    
    Den kollar dessutom ifall den lokalt lagrade datan är likadan som den nya fetchade datan när man laddar om sidan. Ifall de är likadana kommer den nya datan inte lagras.
    (dummyposts.js)
    
    Tog bort exemplet från index.html då den inte längre behövs.

---
+++ dummyposts.js
@@ -0,0 +1,45 @@
+function feedFunction() {
+  var feedPosts = document.getElementById("feed-posts");
+  var parsedData = JSON.parse(localStorage.getItem("fetchedData"));
+  var posts = parsedData.posts;
+
+  posts.forEach((post) => {
+    var postElement = document.createElement("div");
+    var postTitle = document.createElement("h3");
+    var postBody = document.createElement("p");
+    var postTags = document.createElement("ul");
+
+    postElement.classList.add("post-main");
+    postTitle.classList.add("post-title");
+    postBody.classList.add("post-body");
+    postTags.classList.add("post-tags");
+
+    postTitle.textContent = post.title;
+    postBody.textContent = post.body;
+
+    post.tags.forEach((tag) => {
+      var tagItem = document.createElement("li");
+      tagItem.textContent = tag;
+      postTags.append(tagItem);
+    });
+    feedPosts.append(postElement);
+    postElement.append(postTitle, postBody, postTags);
+  });
+}
+
+fetch("https://dummyjson.com/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    var dataString = JSON.stringify(data);
+    var localDataString = localStorage.getItem("fetchedData");
+
+    if (dataString === localDataString) {
+      console.log("Data already fetched");
+      return feedFunction();
+    } else {
+      localStorage.setItem("fetchedData", dataString);
+      console.log("Data stored locally:", data);
+      return feedFunction();
+    }
+  })
+  .catch((error) => console.error("Error fetching data:", error));
